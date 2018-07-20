e2ee0e37f02465e8a3be33abd6ea4d90db85c41d
Sara Heljasvuo <sara.heljasvuo@heltti.fi>
2018-07-20 11:48:48 +0300

    Updated classes to stateless functional component

--- react-course-projects/indecision-app/src/app.js
+++ react-course-projects/indecision-app/src/app.js
@@ -1,148 +1,148 @@
 class IndecisionApp extends React.Component {
     constructor(props) {
-        super(props);
-
-        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-        this.handlePick = this.handlePick.bind(this);
-        this.handleAddOption = this.handleAddOption.bind(this);
-
-        this.state = {
-            options: []
+      super(props);
+      this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+      this.handlePick = this.handlePick.bind(this);
+      this.handleAddOption = this.handleAddOption.bind(this);
+      this.state = {
+        options: props.options
+      };
+    }
+    handleDeleteOptions() {
+      this.setState(() => {
+        return {
+          options: []
         };
+      });
     }
-
-    handleDeleteOptions() { //poistaa kaikki vaihtoehdot
-        this.setState(() => { //funktio
-            return {
-                option: [] //tehdään tyhjä taulukko
-            };
-        });
-    }
-
     handlePick() {
-            const randomNum = Math.floor(Math.random() * this.state.options.length);
-            const option = this.state.options[randomNum];
-            alert(option);
+      const randomNum = Math.floor(Math.random() * this.state.options.length);
+      const option = this.state.options[randomNum];
+      alert(option);
     }
-
     handleAddOption(option) {
-        if (!option) {
-            return 'Enter valid value to add item'
-        } else if (this.state.options.indexOf(option) > -1) {
-            return 'This option already exits';
+      if (!option) {
+        return 'Enter valid value to add item';
+      } else if (this.state.options.indexOf(option) > -1) {
+        return 'This option already exists';
+      }
+  
+      this.setState((prevState) => {
+        return {
+          options: prevState.options.concat(option)
+        };
+      });
+    }
+    render() {
+      const subtitle = 'Put your life in the hands of a computer';
+  
+      return (
+        <div>
+          <Header subtitle={subtitle} />
+          <Action
+            hasOptions={this.state.options.length > 0}
+            handlePick={this.handlePick}
+          />
+          <Options
+            options={this.state.options}
+            handleDeleteOptions={this.handleDeleteOptions}
+          />
+          <AddOption
+            handleAddOption={this.handleAddOption}
+          />
+        </div>
+      );
+    }
+  }
+  
+  IndecisionApp.defaultProps = {
+      options: []
+  };
+
+  const Header = (props) => { //stateless functional component
+    return (
+      <div>
+        <h1>{props.title}</h1>
+        {props.subtitle && <h2>{props.subtitle}</h2>}
+      </div>
+    );
+  };
+
+  Header.defaultProps = { //jos ei ole titlea, otsikkona näkyy Indecision
+      title: 'Indecision'
+  };
+  
+  const Action = (props) => {
+    return (
+      <div>
+        <button
+          onClick={props.handlePick}
+          disabled={!props.hasOptions}
+        >
+          What should I do?
+        </button>
+      </div>
+    );
+  };
+  
+  const Options = (props) => {
+    return (
+      <div>
+        <button onClick={props.handleDeleteOptions}>Remove All</button>
+        {
+          props.options.map((option) => <Option key={option} optionText={option} />)
         }
-
-        this.setState((prevState) => {
-            return {
-                options: prevState.options.concat([option])
-            }
-        })
-    }
-
-    render () {
-        const title = "Indecision";
-        const subtitle = "Put your life in the hands of a computer";
-
-        return (
-            <div>
-                <Header title={title} subtitle={subtitle}/>
-                <Action 
-                    hasOptions={this.state.options.length > 0}
-                    handlePick={this.handlePick}
-                />
-                <Options 
-                    options={this.state.options} //uusi prop, joka saa arvokseen staten
-                    handleDeleteOptions={this.handleDeleteOptions} //uusi prop, joka saa arvokseen funktion
-                />
-                <AddOption
-                    handleAddOption={this.handleAddOption}
-                />
-            </div>
-        );
-    }
-}
-
-class Header extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>{this.props.title}</h1>
-                <h2>{this.props.subtitle}</h2>
-            </div>
-        );
-    }
-}
-
-class Action extends React.Component {
-    render() {
-        return (
-            <div>
-                <button 
-                    onClick={this.props.handlePick}
-                    disabled={!this.props.hasOptions}
-                >
-                    What should I do?
-                </button>
-            </div>
-        );
-    }
-}
-
-class Options extends React.Component {
-    render() {
-        return (
-            <div>
-                <button onClick={this.props.handleDeleteOptions}>Remove All</button> 
-
-                {this.props.options.map((option) => <Option key={option} optionText={option} />)}
-            </div>
-        );
-    }
-}
-
-class Option extends React.Component {
-    render() {
-        return (
-            <div>
-                {this.props.optionText}
-            </div>
-        );
-    }
-}
-
-class AddOption extends React.Component {
+      </div>
+    );
+  };
+  
+  const Option = (props) => {
+    return (
+      <div>
+        {props.optionText}
+      </div>
+    );
+  };
+  
+  class AddOption extends React.Component {
     constructor(props) {
-        super(props);
-
-        this.handleAddOption = this.handleAddOption.bind(this);
-
-        this.state = {
-            error: undefined
-        }
+      super(props);
+      this.handleAddOption = this.handleAddOption.bind(this);
+      this.state = {
+        error: undefined
+      };
     }
     handleAddOption(e) {
-        e.preventDefault();
-
-        const option = e.target.elements.option.value.trim(); //tallentaa syötetyn arvon optioniin, trimmaa tyhjät pois
-        const error = this.props.handleAddOption(option);
-
-        this.setState(() => {
-            return {error};
-        });
+      e.preventDefault();
+  
+      const option = e.target.elements.option.value.trim();
+      const error = this.props.handleAddOption(option);
+  
+      this.setState(() => {
+        return { error };
+      });
     }
-
     render() {
-        return (
-            <div>
-                {this.state.error && <p>{this.state.error}</p>}
-
-                <form onSubmit={this.handleAddOption}>
-                    <input type="text" name="option"/>
-                    <button>Add option</button>
-                </form>
-            </div>
-        );
+      return (
+        <div>
+          {this.state.error && <p>{this.state.error}</p>}
+          <form onSubmit={this.handleAddOption}>
+            <input type="text" name="option" />
+            <button>Add Option</button>
+          </form>
+        </div>
+      );
     }
-}
-
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+  }
+  
+  // const User = (props) => {
+  //   return (
+  //     <div>
+  //       <p>Name: {props.name}</p>
+  //       <p>Age: {props.age}</p>
+  //     </div>
+  //   );
+  // };
+  
+  ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+  
