75236a6b5c90593f6769475b40572d8170b48c15
Sara Heljasvuo <sara.heljasvuo@heltti.fi>
2018-07-23 13:19:42 +0300

    Import npm modules

--- react-course-projects/indecision-app/src/app.js
+++ react-course-projects/indecision-app/src/app.js
@@ -1,14 +1,7 @@
-// import './utils.js';
-// import {square, add} from './utils.js';
-import isSenior, {isAdult, canDrink} from './person.js';
-// import { square, add } from './utils.js';
+// install -> import -> use
 
-// console.log('app.js is running');
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(subtract(100, 82));
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log(isAdult(17));
-console.log(canDrink(25));
-
-console.log(isSenior(70));
+const template = React.createElement('p', {}, 'testing 123');
+ReactDOM.render(template, document.getElementById('app'));
