86910a0dde4b7497f0e1d4925f34f185d1ffac9c
Sara Heljasvuo <sara.heljasvuo@heltti.fi>
2018-07-23 11:22:08 +0300

    Imports and exports

--- react-course-projects/indecision-app/src/app.js
+++ react-course-projects/indecision-app/src/app.js
@@ -1,1 +1,10 @@
-console.log("app.js is running");
+// import './utils.js';
+// import {square, add} from './utils.js';
+import {isAdult, canDrink} from './person.js';
+
+console.log(isAdult(17));
+console.log(canDrink(25));
+
+// console.log("app.js is running");
+// console.log(square(4));
+// console.log(add(5, 4));
---
+++ react-course-projects/indecision-app/src/utils.js
@@ -0,0 +1,12 @@
+console.log('utils.js is running');
+
+const square = (x) => x * x;
+
+const add = (a, b) => a + b;
+
+export {square, add};
+
+const isAdult = 18;
+const canDrink = 18;
+
+export {isAdult, canDrink};
