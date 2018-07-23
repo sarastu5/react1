42abd31c38108e942afdf9cd11eebbf503a9a7ac
Sara Heljasvuo <sara.heljasvuo@heltti.fi>
2018-07-23 09:26:39 +0300

    Local storage

--- react-course-projects/indecision-app/src/playground/counter-example.js
+++ react-course-projects/indecision-app/src/playground/counter-example.js
@@ -6,9 +6,26 @@ class Counter extends React.Component {
         this.handleMinusOne = this.handleMinusOne.bind(this);
         this.handleReset = this.handleReset.bind(this);
         this.state = {
-            count: props.count //tulee Counter.defaultPropseista
+            count: 0
         };
-    } 
+    }
+    
+    componentDidUpdate(prevProps, prevState) {
+        console.log("componentDidUpdate");
+        if (prevState.count !== this.state.count); {
+            localStorage.setItem('count', this.state.count);
+        }
+    }
+
+    componentDidMount() {
+        console.log("componentDidMount");
+        const stringCount = localStorage.getItem("count");
+        const count = parseInt(stringCount, 10);
+
+        if (!isNaN(count)) {
+            this.setState(() => ({count: count}));
+        }
+    }
     
     handleAddOne() {
         this.setState((prevState) => {
