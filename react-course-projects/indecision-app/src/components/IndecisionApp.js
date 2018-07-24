1513fedc3d388b3e2ce514e845b272394b6460f5
Sara Heljasvuo <sara.heljasvuo@heltti.fi>
2018-07-24 14:27:27 +0300

    Added babel plugin. Changed event handlers to arrow functions

--- react-course-projects/indecision-app/src/components/IndecisionApp.js
+++ react-course-projects/indecision-app/src/components/IndecisionApp.js
@@ -7,96 +7,92 @@ import Options from './Options.js';
 
 //ei ole oikeutta lifecycle metodeihin
 export default class IndecisionApp extends React.Component {
-    constructor(props) {
-      super(props);
-      this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-      this.handlePick = this.handlePick.bind(this);
-      this.handleAddOption = this.handleAddOption.bind(this);
-      this.handleDeleteOption = this.handleDeleteOption.bind(this);
-      this.state = {
-        options: props.options
-      };
+  state = {
+    options: []
+  };
+
+  // eventhandlerit ensin
+
+  // muutetaan eventhandlerit arrow funktioiksi
+  handleDeleteOptions = () => {
+      this.setState(() => ({options: []}));
+  };
+
+  handleDeleteOption = (optionToRemove) => {
+      this.setState((prevState) => ({
+         options: prevState.options.filter((option) => optionToRemove !== option)
+      }));
+  };
+
+  handlePick = () => {
+    const randomNum = Math.floor(Math.random() * this.state.options.length);
+    const option = this.state.options[randomNum];
+    alert(option);
+  };
+
+  handleAddOption = (option) => {
+    if (!option) {
+      return 'Enter valid value to add item';
+    } else if (this.state.options.indexOf(option) > -1) {
+      return 'This option already exists';
     }
+
+    this.setState((prevState) => ({
+        options: prevState.options.concat(option)
+    }));
+  };
 
     //lifecycle methods
-    componentDidMount() {
-        try {
-            const json = localStorage.getItem("options");
-            const options = JSON.parse(json);
+  componentDidMount() {
+      try {
+          const json = localStorage.getItem("options");
+          const options = JSON.parse(json);
 
-            if (options) {
-                this.setState(() => ({options: options}));
-            }
-        } catch(e) {
-            //nothing at all
-        }
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        //tarkistetaan, onko options-taulukon pituus muuttunut
-        if (prevState.options.length !== this.state.options.length) {
-            const json = JSON.stringify(this.state.options);
-            localStorage.setItem("options", json);
-        }
-    }
-
-    componentWillUnmount() {
-        console.log("componentWillUnmount");
-        //vaihdetaan sivua tms eli kun ko. komponentti "häviää"
-    }
-
-    handleDeleteOptions() {
-        this.setState(() => ({options: []}));
-    }
-
-    handleDeleteOption(optionToRemove) {
-        this.setState((prevState) => ({
-           options: prevState.options.filter((option) => optionToRemove !== option)
-        }));
-    }
-
-    handlePick() {
-      const randomNum = Math.floor(Math.random() * this.state.options.length);
-      const option = this.state.options[randomNum];
-      alert(option);
-    }
-
-    handleAddOption(option) {
-      if (!option) {
-        return 'Enter valid value to add item';
-      } else if (this.state.options.indexOf(option) > -1) {
-        return 'This option already exists';
+          if (options) {
+              this.setState(() => ({options: options}));
+          }
+      } catch(e) {
+          //nothing at all
       }
-
-      this.setState((prevState) => ({
-          options: prevState.options.concat(option)
-      }));
-    }
-
-    render() {
-      const subtitle = 'Put your life in the hands of a computer';
-  
-      return (
-        <div>
-          <Header subtitle={subtitle} />
-          <Action
-            hasOptions={this.state.options.length > 0}
-            handlePick={this.handlePick}
-          />
-          <Options
-            options={this.state.options}
-            handleDeleteOptions={this.handleDeleteOptions}
-            handleDeleteOption={this.handleDeleteOption}
-          />
-          <AddOption
-            handleAddOption={this.handleAddOption}
-          />
-        </div>
-      );
-    }
   }
 
-  IndecisionApp.defaultProps = {
+  componentDidUpdate(prevProps, prevState) {
+      //tarkistetaan, onko options-taulukon pituus muuttunut
+      if (prevState.options.length !== this.state.options.length) {
+          const json = JSON.stringify(this.state.options);
+          localStorage.setItem("options", json);
+      }
+  }
+
+  componentWillUnmount() {
+      console.log("componentWillUnmount");
+      //vaihdetaan sivua tms eli kun ko. komponentti "häviää"
+  }
+
+  render() {
+    const subtitle = 'Put your life in the hands of a computer';
+
+    return (
+      <div>
+        <Header subtitle={subtitle} />
+        <Action
+          hasOptions={this.state.options.length > 0}
+          handlePick={this.handlePick}
+        />
+        <Options
+          options={this.state.options}
+          handleDeleteOptions={this.handleDeleteOptions}
+          handleDeleteOption={this.handleDeleteOption}
+        />
+        <AddOption
+          handleAddOption={this.handleAddOption}
+        />
+      </div>
+    );
+  }
+}
+
+IndecisionApp.defaultProps = {
     options: []
 };
 
