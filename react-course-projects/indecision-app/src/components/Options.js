bf8535321a4d8fc6da1d289622bdbe1adabaa814
Sara Heljasvuo <sara.heljasvuo@heltti.fi>
2018-07-26 10:02:25 +0300

    Styled option item

--- react-course-projects/indecision-app/src/components/Options.js
+++ react-course-projects/indecision-app/src/components/Options.js
@@ -3,16 +3,25 @@ import Option from './Option.js';
 
 const Options = (props) => (
   <div>
-    <button onClick={props.handleDeleteOptions}>Remove All</button>
+    <div className="widget-header">
+      <h3 className="widget-header__title">Your Options</h3>
+      <button 
+        className="button button--link"
+        onClick={props.handleDeleteOptions}
+        >
+          Remove All
+      </button>
+    </div>
 
     {/*Jos taulukko on tyhjä, näkyy tää*/}
-    {props.options.length == 0 && <p>Please add an option to get started!</p>}
+    {props.options.length == 0 && <p className="widget__message">Please add an option to get started!</p>}
 
     {
-      props.options.map((option) => (
+      props.options.map((option, index) => (
           <Option 
             key={option} 
-            optionText={option} 
+            optionText={option}
+            count={index + 1}
             handleDeleteOption={props.handleDeleteOption}
           />
       ))
