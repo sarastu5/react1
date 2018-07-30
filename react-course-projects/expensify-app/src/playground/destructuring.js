72a246afe1f868534ac3bca3a0e7f73e5057c73e
Sara Heljasvuo <sara.heljasvuo@heltti.fi>
2018-07-30 13:49:04 +0300

    Array destructuring

--- react-course-projects/expensify-app/src/playground/destructuring.js
+++ react-course-projects/expensify-app/src/playground/destructuring.js
@@ -1,3 +1,7 @@
+//
+//Object destrutring
+//
+
 // const person = {
 //     name: 'Sara',
 //     age: 30,
@@ -19,16 +23,31 @@
 //     console.log(`It's ${temperature} degrees celsius in ${city}.`);
 // }
 
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-        name: 'Penguin'
-    }
-};
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// };
 
-const {title, author} = book;
-const {name: publisherName = 'Self-publisched'} = book.publisher;
+// const {title, author} = book;
+// const {name: publisherName = 'Self-publisched'} = book.publisher;
 
-console.log(publisherName);
+// console.log(publisherName);
 
+//
+//Array destructuring
+//
+
+// const address = ['Mannerheimintie 12', 'Helsinki', 'Suomi', '00100'];
+
+// const [street, city, country, zip] = address;
+
+// console.log(`You are in ${city} ${country}.`);
+
+const item = ['Coffee (hot)', '2 euros', '2,5 euros', '3 euros'];
+
+const [itemName, , mediumPrice] = item;
+
+console.log(`A medium ${itemName} costs ${mediumPrice}`);
