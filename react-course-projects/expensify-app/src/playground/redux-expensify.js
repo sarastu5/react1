a0705f3c472aab121bfb0de95b7fb477e4b3ba87
Sara Heljasvuo <sara.heljasvuo@heltti.fi>
2018-07-31 09:11:26 +0300

    Added multiple reducers

--- react-course-projects/expensify-app/src/playground/redux-expensify.js
+++ react-course-projects/expensify-app/src/playground/redux-expensify.js
@@ -1,4 +1,52 @@
 import {createStore, combineReducers} from 'redux';
+import { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } from 'constants';
+
+// ADD_EXPENSE
+// REMOVE_EXPENSE
+// EDIT:EXPENSE
+// SET_TEXT_FILTER
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_END_DATE
+
+// Expenses Reducer
+
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+    switch (action.type) {
+        default:   
+            return state;
+    }
+};
+
+// Filters Reducer
+
+const filtersReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+};
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+    switch (action.type) {
+        default: 
+            return state;
+    }
+};
+
+// Store creation
+
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filters: filtersReducer
+    })
+);
+
+console.log(store.getState());
 
 const demoState = {
     expenses: [{
