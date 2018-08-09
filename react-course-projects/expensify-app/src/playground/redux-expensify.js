1dc2b6aa41bb417c6c1b640ba6a590d7e0ec372b
Sara Heljasvuo <sara.heljasvuo@heltti.fi>
2018-08-09 11:07:29 +0300

    Setting up all over again. Filtering redux data

--- react-course-projects/expensify-app/src/playground/redux-expensify.js
+++ react-course-projects/expensify-app/src/playground/redux-expensify.js
@@ -40,9 +40,30 @@ const setTextFilter = (text = '') => ({
 });
 
 // SORT_BY_DATE
+
+const sortByDate = () => ({
+    type: 'SORT_BY_DATE'
+});
 // SORT_BY_AMOUNT
+
+
+const sortByAmount = () => ({
+    type: 'SORT_BY_AMOUNT'
+});
+
 // SET_START_DATE
+
+const setStartDate = (startDate) => ({
+    type: 'SET_START_DATE',
+    startDate
+});
+
 // SET_END_DATE
+
+const setEndDate = (endDate) => ({
+    type: 'SET_END_DATE',
+    endDate
+});
 
 // Expenses Reducer
 
@@ -89,9 +110,43 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
                 ...state,
                 text: action.text
             }
+        case 'SORT_BY_AMOUNT':
+            return {
+                ...state,
+                sortBy: 'amount'
+            };
+        case 'SORT_BY_DATE':
+            return {
+                ...state,
+                sortBy: 'date'
+            };
+        case 'SET_START_DATE':
+            return {
+                ...state,
+                startDate: action.startDate
+            };
+        case 'SET_END_DATE':
+            return {
+                ...state,
+                endDate: action.endDate
+            };
         default: 
             return state;
     }
+};
+
+// timestamps (milliseconds) (pos tai neg int values)
+
+// Get visible expenses
+const getVisibleExpenses = (expenses, {text, sortBy, startDate, endDate}) => {
+    return expenses.filter((expense) => {
+        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+        const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+        const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+        // kaikkien pitää olla totta:
+        return startDateMatch && endDateMatch && textMatch;
+    });
 };
 
 // Store creation
@@ -105,15 +160,25 @@ const store = createStore(
 
 // seuraa muutoksia
 store.subscribe(() => {
-    console.log(store.getState());
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log(visibleExpenses);
 });
 
-const expenseOne = store.dispatch(addExpense({description: 'Rent', amount: 1000}));
-const expenseTwo = store.dispatch(addExpense({description: 'Coffee', amount: 2}));
+const expenseOne = store.dispatch(addExpense({description: 'Rent', amount: 300, createdAt: 1000}));
+const expenseTwo = store.dispatch(addExpense({description: 'Coffee', amount: 300, createdAt: -1000}));
 
-store.dispatch(removeExpense({id: expenseOne.expense.id}));
-store.dispatch(editExpense(expenseTwo.expense.id, {amount: 5}));
-store.dispatch(setTextFilter('rent'));
+// store.dispatch(removeExpense({id: expenseOne.expense.id}));
+// store.dispatch(editExpense(expenseTwo.expense.id, {amount: 5}));
+
+store.dispatch(setTextFilter('ffe'));
+
+// store.dispatch(sortByAmount());
+// store.dispatch(sortByDate());
+
+// store.dispatch(setStartDate(0));
+// store.dispatch(setStartDate());
+// store.dispatch(setEndDate(999));
 
 const demoState = {
     expenses: [{
@@ -131,12 +196,12 @@ const demoState = {
     }
 };
 
-const user = {
-    name: 'Jen',
-    age: 24
-};
+// const user = {
+//     name: 'Jen',
+//     age: 24
+// };
 
-console.log({
-    ...user, //ottaa userista tiedot ja lisää locationin niiden perään
-    location: 'Helsinki'
-})
+// console.log({
+//     ...user, //ottaa userista tiedot ja lisää locationin niiden perään
+//     location: 'Helsinki'
+// })
