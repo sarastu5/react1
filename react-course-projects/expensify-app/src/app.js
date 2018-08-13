5452ab109cef3e368b8999908aa7a2aa1601d793
Sara Heljasvuo <sara.heljasvuo@heltti.fi>
2018-08-13 11:03:21 +0300

    Sorting items

--- react-course-projects/expensify-app/src/app.js
+++ react-course-projects/expensify-app/src/app.js
@@ -11,13 +11,9 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill' }));
-store.dispatch(addExpense({ description: 'Gas bill' }));
-store.dispatch(setTextFilter('water'));
-
-setTimeout(() => {
-    store.dispatch(setTextFilter('rent'));
-}, 3000);
+store.dispatch(addExpense({ description: 'Water bill', amount: 450 }));
+store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'Rent', amount: 700 }));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
